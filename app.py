fdead587b66409d95b2ea93466ae330fce65212e
Rajendraprasath <59873620+SSRaj001@users.noreply.github.com>
2021-03-31 10:52:50 +0530

    login part

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# Initialize
+db = SQLAlchemy(app)
+
+
+# model
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
 
 
 @app.route('/')
@@ -16,6 +31,14 @@ def login():
     else:
         uname = request.form['username']
         password = request.form['password']
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html', err="GET")
+    else:
+        return render_template('signup.html', err="POST")
 
 
 if __name__ == '__main__':
