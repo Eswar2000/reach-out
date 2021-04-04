a81dec4662211b30823f801861326c5ac347ee52
Eswar2000 <v.eswarraman2000@gmail.com>
2021-04-04 14:43:48 +0530

    dashboard connection made

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Thestylishstar1'
+app.config['MYSQL_PASSWORD'] = 'pieceofshit'
 app.config['MYSQL_DB'] = 'test_db'
 
 mysql.init_app(app)
@@ -30,10 +30,8 @@ def login():
         temp = cursor.fetchall()
         mysql.connection.commit()
         cursor.close()
-        print(temp)
-        
-        if(check_password_hash(temp[0][1],password)):
-            return render_template('dashboard.html',user=temp[0][0])
+        if(check_password_hash(temp[0][2],password)):
+            return render_template('dashboard.html',user=temp[0][1])
         else:
             return render_template('login.html',err="Username Or Password Is Wrong")
     else:
